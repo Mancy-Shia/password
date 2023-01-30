e1179b01e4ef5096322d76b919de7ea82442651e
Mancy <clare99048@gmail.com>
2023-01-30 14:24:18 +0800

    add new file

---
+++ password.py
@@ -0,0 +1,10 @@
+i = 3
+i = int(i)
+while i:
+    password = input()
+    i = i - 1
+    if password == 'a123456':
+        print("登入成功")
+        break
+    else:
+        print('密碼錯誤! 還有', i, '次機會')
